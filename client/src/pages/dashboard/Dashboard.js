99b7d9ab2604d793937037193c46038b5e96a567
iuaa <heteai17@gmail.com>
2023-05-26 16:30:28 +0300

    some fixed error

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,13 +1,33 @@
-import React from "react";
 import "./Dashboard.css";
-import Navbar from "../../components/Navbar";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
 function Dashboard() {
   return (
-    <div id="content">
-      <header></header>
-
-      <div className="mainAction"></div>
-    </div>
+    <form>
+      <div className="space">View your flats and appliances for each flat!</div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Flat
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Description flat(address)
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            View
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
   );
 }
 
